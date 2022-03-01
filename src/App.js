0ffbc183f655589c9be45dad49e760a7c33eb763
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-01 19:37:52 +0200

    Import The Store in the src/App.js file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import BookstoreContainer from './components/BookstoreContainer';
 import Header from './components/Header';
 import Categories from './redux/categories/categories';
 
 const App = () => (
-  <div className="App">
-    <Header />
-    <Routes>
-      <Route path="/" element={<BookstoreContainer />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
-  </div>
+  <Provider store={store}>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<BookstoreContainer />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
+  </Provider>
 );
 
 export default App;
