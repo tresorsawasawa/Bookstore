88eaff3c1c0308525cd29299ebca71898776f721
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-01 19:35:04 +0200

    Implement the 'Store' file and some logic

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,1 +1,11 @@
-// import { createStore, combineReducers,  } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import booksReducer from './books/books';
+
+const rootReducer = combineReducers({
+  booksReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+export default store;
